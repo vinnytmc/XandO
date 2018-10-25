640673e00e106d33e4cdb036f349df23a3d3dae0
vinnytmv <valopez@mail.csuchico.edu>
2018-10-24 18:00:27 -0700

    X and Os

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 //Author:
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
@@ -15,8 +16,8 @@ int main()
 {
   //3x3 matrix of characters, initially with blank spaces in each position
   char board[ROWS][COLUMNS] = {{BLANK, BLANK, BLANK},
-                               {BLANK, BLANK, BLANK},
-                               {BLANK, BLANK, BLANK}};
+    {BLANK, BLANK, BLANK},
+    {BLANK, BLANK, BLANK}};
 
   char turn = X; //Player X always goes first
   int row;
@@ -40,27 +41,48 @@ int main()
     {
       cout<<"Values must be between 0 and 2.\n";
     }
-    //TODO: Place the piece and swap turns
-    //Outline
-    //1. When neither of the (above) if nor else if statements are true, then...
-    //1-A.  Assign the board location of row and column to the value of turn
-    //1-B.  When turn has the value 'X', then...
-    //1-B-1.  turn should be assigned the value 'O'
-    //1-C.  In any other case, then...
-    //1-C-1.  turn should be assigned the value 'X'
-    
-    cout<<"\nBOARD\n-----\n";
-    //TODO: Print the current board
-    //Outline
-    //1. Traverse through each row, calling the current row r
-    //1-A. Traverse through each column in row r, calling the current column c
-    //1-A-1.  Display the value of the board at location of r and c
-    //1-A-2. Display a space
-    //1-B. Display an newline to move to the next row of the board
-    
-  }while( playing );
+    else
+    {
+      board[row][column] = turn;
 
-  cout<<"Goodbye!\n";
+      if(turn == X)
+      {
+        turn = O;
+      }
+      else
+      {
+        turn = X;
+      }
 
-  return 0;
+      //TODO: Place the piece and swap turns
+      //Outline
+      //1. When neither of the (above) if nor else if statements are true, then...
+      //1-A.  Assign the board location of row and column to the value of turn
+      //1-B.  When turn has the value 'X', then...
+      //1-B-1.  turn should be assigned the value 'O'
+      //1-C.  In any other case, then...
+      //1-C-1.  turn should be assigned the value 'X'
+
+      cout<<"\nBOARD\n-----\n"; 
+      //TODO: Print the current board
+      //Outline
+      //1. Traverse through each row, calling the current row r
+      //1-A. Traverse through each column in row r, calling the current column c
+      //1-A-1.  Display the value of the board at location of r and c
+      //1-A-2. Display a space
+      //1-B. Display an newline to move to the next row of the board
+      for(int r=0; r < ROWS; r++)
+      {
+        for(int c=0; c < COLUMNS; c++)
+        {
+          cout<<board[r][c]<<" ";
+        }
+        cout<<endl;
+      }
+    }
+    }while( playing );
+  
+    cout<<"Goodbye!\n";
+
+    return 0;
 }
